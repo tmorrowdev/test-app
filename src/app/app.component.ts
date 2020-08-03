4ae062bc34fc483b68742915dfa19223d026ba04
Tyler Morrow <tsmorro@gmail.com>
2020-08-03 15:08:36 -0400

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  public label = 'Select a Product';
+  public search = false;
+  public placeholder = 'Select';
   public disabled = false;
   public content = 'Button';
   public color = 'costar';
   public type = 'contained';
   public icon = 'map';
-  public products = [
-		{ 	Value : "product 1",
-			ID : 1
-		},
-		{	Value : "product 2",
-			ID : 2
-		},
-		{	Value : "product 3",
-			ID : 3
-		}
-	];
+  public pagesize = 10;
+  public titlemetric = 'Tasks Completed';
+	public subtitle = 'by the end of the week';
+	public heroMetric = '204,000';
+  public sidekickMetric = '-34';
+  public sidekickColor = 'red';
+  public tooltip = 'Tasks Completed by week\'s end';
+  public titlemetric2 = 'Tasks Completed';
+	public subtitle2 = 'by the end of the week';
+	public heroMetric2 = '$204,000 MM';
+  public sidekickMetric2 = '-34';
+  public sidekickColor2 = 'red';
+  public tooltip2 = 'Tasks Completed by week\'s end';
+  public icons = 'info';
+  public listItems =[
+    { 	name : 'item 1',
+      id : 1
+    },
+    {	name : 'item 2',
+      id : 2
+    },
+    {	name : 'item 3',
+      id : 3
+    }
+  ];
+
 }
